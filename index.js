74db5693d86b1c57e93e5a64c67f7f4b23cf8fa0
Ryan Kramer <rkramer613@gmail.com>
2017-03-18 10:42:30 -0600

    Added functions

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+db.new_plane(function(err, planes){
+    console.log(err, "plane added")
+});
+
+db.get_planes(function(err, planes){
+    console.log(err, planes)
+});
+
+
+
+
 app.listen('3000', function(){
   console.log("Successfully listening on : 3000")	
 })
