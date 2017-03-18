b669d229c61382d064086fd98360e179b84466a7
Ryan Kramer <rkramer613@gmail.com>
2017-03-18 10:35:07 -0600

    DB

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var massive = require('massive');
+var connectionString = "Ryan:Limitless-2017!@localhost/sandbox";
+
+var massiveInstance = massive.connectSync({connectionString: connectionString})
+app.set('db', massiveInstance);
+
+var db = app.get('db');
 
 var app = module.exports = express();
 app.use(bodyParser.json());
