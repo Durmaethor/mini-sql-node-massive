84b6ca9d3d011a72b21ff02d088457df8f97651a
Ryan Kramer <rkramer613@gmail.com>
2017-03-18 10:02:29 -0600

    added massive, required

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
 
 var app = module.exports = express();
 app.use(bodyParser.json());
